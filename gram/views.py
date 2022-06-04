fc686335d40164991d7ffc6580c81145db3c3128
Moryn <moureen.chepkoech@student.moringaschool.com>
2022-06-05 02:56:41 +0300

    create a login function

--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,10 @@
+from .models import ImageField
 from django.shortcuts import render
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+    profile = Profile.objects.all()
+    posts = ImageField.objects.all()
+
+    return render(request,'home.html',{"profile":profile, "posts":posts})
