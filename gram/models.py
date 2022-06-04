c84f52a36516d26a1cd1974ac7f9c231c7b2fdd6
Moryn <moureen.chepkoech@student.moringaschool.com>
2022-06-05 02:41:53 +0300

    create an image model

--- gram/models.py
+++ gram/models.py
@@ -25,3 +25,25 @@ class Profile(models.Model):
     @classmethod
     def search_profile(cls, name):
         return cls.objects.filter(user__username__icontains=name).all()
+
+#class Images
+class Image(models.Model):
+    image = CloudinaryField('images')
+    image_name = models.CharField(max_length=30,blank=True)
+    image_caption = models.CharField(max_length=300)
+    profile = models.ForeignKey(Profile,on_delete = models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+    comments = models.CharField(max_length=30,blank=True)
+
+    @classmethod
+    def update_image_caption(cls, id, image_caption):
+        cls.objects.filter(id=id).update(image_caption=image_caption)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()  
+
+    def __str__(self):
+        return self.image_name          
