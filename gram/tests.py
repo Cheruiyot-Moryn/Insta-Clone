9e64f9e2b78aca3c6b8b8e3cd3efecb302d86f28
Moryn <moureen.chepkoech@student.moringaschool.com>
2022-06-05 03:13:16 +0300

    test for user profile model

--- gram/tests.py
+++ gram/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+# test for model class Profile
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user("testuser", "secret")
+        self.profile_test = Profile(picture='test.jpg',bio="this is a test bio",user=self.user)
+        self.profile_test.save()
+
+    def test_instance_true(self):
+        self.profile_test.save()
+        self.assertTrue(isinstance(self.profile_test, Profile))
+
+    def test_save_profile(self):
+        self.profile_test.save()
+        pp = Profile.objects.all()
+        self.assertTrue(len(pp) > 0)
